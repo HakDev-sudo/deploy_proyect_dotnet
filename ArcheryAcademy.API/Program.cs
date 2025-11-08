6012ea72f9b1178cd5aae5d1657cc5681fb79dfd
jeefry.zamata@tecsup.edu.pe <jeefry.zamata@tecsup.edu.pe>
2025-11-08 17:02:18 -0500

    "modify: Clean Program (Weather)"

--- ArcheryAcademy.API/Program.cs
+++ ArcheryAcademy.API/Program.cs
@@ -1,41 +1,35 @@
+using ArcheryAcademy.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//Register all services  for extension method
+builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// use Swagger
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Swagger en la raíz
+    });
 }
 
-app.UseHttpsRedirection();
+// Routing
+app.UseRouting();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Middlewares personalizados 
+//app.UseMiddleware<ErrorHandlingMiddleware>();           
+//app.UseMiddleware<ParameterValidationMiddleware>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Autenticación y Autorización
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+// Controllers
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
