e38a53d4b76096dd859cd9686270f2392ed56ae9
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2025-11-08 16:16:14 -0500

    refactor(domain): move entities from Infrastructure to Domain layer

---
+++ ArcheryAcademy.Domain/Entities/UserPlan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcheryAcademy.Infrastructure.Persistence.Models;
+
+public partial class UserPlan
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int PlanId { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public int? RemainingClasses { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual Plan Plan { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
