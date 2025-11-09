c4923bd9bc0ddb9ba7701232c3e659d431acfbbb
jeefry.zamata@tecsup.edu.pe <jeefry.zamata@tecsup.edu.pe>
2025-11-08 19:54:33 -0500

    feat: Implement Schedule GetAll (controller and GetAllSchedulesQuery)

---
+++ ArcheryAcademy.API/Controllers/ScheduleController.cs
@@ -0,0 +1,19 @@
+using ArcheryAcademy.Application.DTOs.ScheduleDto;
+using ArcheryAcademy.Application.UseCases.ScheduleUseCase.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcheryAcademy.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ScheduleController(IMediator mediator) : ControllerBase
+{
+    // GET: api/schedule
+    [HttpGet]
+    public async Task<ActionResult<List<ScheduleReadDto>>> GetAll()
+    {
+        var result = await mediator.Send(new GetAllSchedulesQuery());
+        return Ok(result);
+    }
+}
