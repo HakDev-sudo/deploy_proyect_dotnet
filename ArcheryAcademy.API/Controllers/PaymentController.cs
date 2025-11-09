d62bfd4c97b5d959443350880a77beb2e1436d9b
Deyson34 <deyson.flores@tecsup.edu.pe>
2025-11-08 20:57:19 -0500

    feat: Implement Payment GetAll (controller and GetAllPaymentQuery)

---
+++ ArcheryAcademy.API/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+using ArcheryAcademy.Application.Dtos.PaymentDto;
+using ArcheryAcademy.Application.UseCases.PaymentUseCases.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcheryAcademy.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentController(IMediator mediator) : ControllerBase
+{
+    // GET: api/Payment
+    [HttpGet]
+    public async Task<ActionResult<List<PaymentReadDto>>> GetAll()
+    {
+        var result = await mediator.Send(new GetAllPaymentsQuery()); 
+        
+        return Ok(result);
+    }
+}
