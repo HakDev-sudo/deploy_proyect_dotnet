d849b4af170bf78aa7a03ab49ef7b2106a801c1a
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2025-11-08 20:03:35 -0500

    feat: implement GetUserPlanById query and endpoint

--- ArcheryAcademy.API/Controllers/UserPlansController.cs
+++ ArcheryAcademy.API/Controllers/UserPlansController.cs
@@ -1,4 +1,5 @@
 using ArcheryAcademy.Application.DTOs.UserPlanDto;
+using ArcheryAcademy.Application.UseCases.UserPlanUseCases.Commands;
 using ArcheryAcademy.Application.UseCases.UserPlanUseCases.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,26 @@ public class UserPlansController(IMediator mediator) : ControllerBase
     public async Task<ActionResult<List<UserPlanReadDto>>> GetAll()
     {
         var result = await mediator.Send(new GetAllUserPlansQuery());
+        return Ok(result);
+    }
+    
+    // GET by id
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var result = await mediator.Send(new GetUserPlanByIdQuery(id));
+        if (result == null)
+            return NotFound(new { message = $"UserPlan with ID {id} not found." });
+
+        return Ok(result);
+    }
+    
+    // post 
+    [HttpPost]
+    public async Task<IActionResult> CreateUserPlan([FromBody] UserPlanCreateDto dto)
+    {
+        var command = new CreateUserPlanCommand(dto);
+        var result = await mediator.Send(command);
         return Ok(result);
     }
 }
